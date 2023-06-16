105660589e938affa319ce38da98e79dfc499e36
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-06-16 08:41:13 -0600

    moviesRpositoryProvider implemented with riverpod

--- lib/infrastructure/repositories/movies_repository_impl.dart
+++ lib/infrastructure/repositories/movies_repository_impl.dart
@@ -2,11 +2,11 @@ import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/domain/repositories/movies_repository.dart';
 
-class MovieRepositoryImpl extends MoviesRepository {
+class MoviesRepositoryImpl extends MoviesRepository {
 
   final MoviesDatasource datasource;
 
-  MovieRepositoryImpl(this.datasource);
+  MoviesRepositoryImpl(this.datasource);
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) {
