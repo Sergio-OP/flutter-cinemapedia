cfc414d59572be75fe1aba0655ac498cfdbcbc10
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-06-17 07:43:45 -0600

    visualize movies list now playing

--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -2,4 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/infrastructure/repositories/movies_repository_impl.dart';
 import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
 
-final moviesRepositoryProvider = Provider((ref) => MoviesRepositoryImpl(MoviedbDatasource()));
+final moviesRepositoryProvider = Provider((ref) {
+  return MoviesRepositoryImpl(MoviedbDatasource());
+});
