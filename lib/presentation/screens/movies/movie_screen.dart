186876e878b534329093e206fe07faecc46dce01
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-07-01 20:38:04 -0600

    actor list and animations added

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie-screen';
@@ -109,7 +110,7 @@ class _MovieDetails extends StatelessWidget {
           ),
         ),
 
-        // TODO: mostrar actores en listview
+        // show actors
         _ActorsByMovie(movieid: movie.id.toString()),
 
         const SizedBox(height: 10,)
@@ -140,6 +141,7 @@ class _ActorsByMovie extends ConsumerWidget {
         itemCount: actors.length,
         itemBuilder: (context, index) {
           final actor = actors[index];
+
           return Container(
             padding: const EdgeInsets.all(8.0),
             width: 135,
@@ -147,13 +149,15 @@ class _ActorsByMovie extends ConsumerWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 // Actor photo
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.network(
-                    actor.profilePath,
-                    height: 180,
-                    width: 135,
-                    fit: BoxFit.cover,
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
 
@@ -193,17 +197,21 @@ class _CustomSilverAppbar extends StatelessWidget {
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        title: Text(
+        /*title: Text(
           movie.title,
           style: const TextStyle(fontSize: 20),
           textAlign: TextAlign.start,
-        ),
+        ),*/
         background: Stack(
           children: [
             SizedBox.expand(
               child: Image.network(
                 movie.posterPath,
                 fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if(loadingProgress != null) return const SizedBox();
+                  return FadeIn(child: child);
+                },
               ),
             ),
 
