2bfc3b38debcee6a794105b0a8e515e948a11218
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-07-01 19:05:18 -0600

    actor mapper from db to domain entity

---
+++ lib/infrastructure/mappers/actor_mapper.dart
@@ -0,0 +1,15 @@
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/credits_response.dart';
+
+class ActorMapper {
+
+  static Actor castFromDbToEntity(Cast cast) => Actor(
+    id: cast.id, 
+    name: cast.name, 
+    profilePath: cast.profilePath != null
+      ? 'https://image.tmdb.org/t/p/w500${cast.profilePath}'
+      : 'https://wallpapercave.com/wp/wp9566480.png',
+    character: cast.character
+  );
+  
+}
