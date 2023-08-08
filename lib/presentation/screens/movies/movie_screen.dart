54a184373c9a71d205a30db72e188d66540e4fb0
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-08-07 20:08:16 -0600

    toggleFavorite implemented and added

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 
 class MovieScreen extends ConsumerStatefulWidget {
@@ -182,13 +181,13 @@ class _ActorsByMovie extends ConsumerWidget {
   }
 }
 
-class _CustomSilverAppbar extends StatelessWidget {
+class _CustomSilverAppbar extends ConsumerWidget {
   final Movie movie;
 
   const _CustomSilverAppbar({required this.movie});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final size = MediaQuery.of(context).size;
 
     return SliverAppBar(
@@ -196,8 +195,14 @@ class _CustomSilverAppbar extends StatelessWidget {
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
       actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_border)),
-        IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_rounded, color: Colors.red,))
+        IconButton(
+          onPressed: () {
+            ref.watch(localStorageRepositoryProvider)
+              .toggleFavorite(movie);
+          }, 
+          icon: const Icon(Icons.favorite_border),
+          // icon: const Icon(Icons.favorite_rounded, color: Colors.red,)
+        ),
       ],
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
