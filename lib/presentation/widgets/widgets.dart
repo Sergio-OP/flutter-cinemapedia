f66a3711a86e026e81c275f97ce23a4f31477c89
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-09-17 18:01:44 -0600

    masonry lib added and favorites view modified

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,3 +1,5 @@
+export 'movies/movie_masonry.dart';
+export 'movies/movie_poster_link.dart';
 export 'shared/custom_appbar.dart';
 export 'movies/movies_slide_show.dart';
 export 'shared/custom_bottom_navigation_bar.dart';
