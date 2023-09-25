515f74b684d85b12009c4385509b0b68f4b5b591
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-09-25 15:38:36 -0600

    Movie Slideshow redirect

--- lib/presentation/widgets/movies/movies_slide_show.dart
+++ lib/presentation/widgets/movies/movies_slide_show.dart
@@ -1,6 +1,6 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
+import 'package:go_router/go_router.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 class MoviesSlideShow extends StatelessWidget {
@@ -57,17 +57,13 @@ class _Slide extends StatelessWidget {
         decoration: decoration,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: Image.network(
-            movie.backdropPath,
-            fit: BoxFit.cover,
-            loadingBuilder: (context, child, loadingProgress) {
-              if(loadingProgress!=null){
-                return const DecoratedBox(
-                  decoration: BoxDecoration(color: Colors.black12)
-                  );
-              }
-              return FadeIn(child: child);
-            },
+          child: GestureDetector(
+            onTap: () => context.push('/home/0/movie/${movie.id}'),
+            child: FadeInImage(
+              placeholder: const AssetImage('assets/loaders/bottle-loader.gif'),
+              image: NetworkImage(movie.backdropPath),
+              fit: BoxFit.cover,
+            ),
           ),
           ),
       ),
