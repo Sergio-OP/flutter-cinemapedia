701e9cba4f2e5fe903a3e4e2970da515ab3a3af6
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2023-09-25 11:13:02 -0600

    no favorite movies screen added

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -11,10 +12,10 @@ class FavoritesView extends ConsumerStatefulWidget {
 }
 
 class FavoritesViewState extends ConsumerState<FavoritesView> {
-
+  
   bool isLastPage = false;
   bool isLoading = false;
-  
+
   @override
   void initState() {
     super.initState();
@@ -23,8 +24,36 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
 
   @override
   Widget build(BuildContext context) {
-
     final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    if (favoriteMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.favorite_outline_sharp,
+              size: 60,
+              color: colors.primary,
+            ),
+            Text('Ohhh no!',
+                style: TextStyle(fontSize: 30, color: colors.primary)),
+            const Text(
+              'You don\'t have favorite movies',
+              style: TextStyle(fontSize: 20, color: Colors.black45),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            FilledButton.tonal(
+                onPressed: () => context.go('/home/0'),
+                child: const Text('Start looking'))
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       body: MovieMasonry(
@@ -35,16 +64,15 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   }
 
   void loadNextPage() async {
-    if(isLoading || isLastPage) return;
+    if (isLoading || isLastPage) return;
     isLoading = true;
 
-    final movies = await ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+    final movies =
+        await ref.read(favoriteMoviesProvider.notifier).loadNextPage();
     isLoading = false;
 
-    if(movies.isEmpty){
+    if (movies.isEmpty) {
       isLastPage = true;
     }
-
   }
-
 }
